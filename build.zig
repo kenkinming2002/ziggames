d3e9b8d862d57ea1b894dc775059e8e4619012a1
Kwok Kin Ming <kenkinming2002@gmail.com>
2024-12-14 20:51:20 +0800

    Setup infrastructure for multiple output executables

--- build.zig
+++ build.zig
@@ -4,17 +4,20 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "ziggames",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    for ([_][]const u8{"breakout"}) |name| {
+        const root_source_file = b.path(b.fmt("src/{s}.zig", .{name}));
+        const exe = b.addExecutable(.{
+            .name = name,
+            .root_source_file = root_source_file,
+            .target = target,
+            .optimize = optimize,
+        });
 
-    exe.linkLibC();
-    exe.addIncludePath(b.path("./raylib-5.5_linux_amd64/include"));
-    exe.addLibraryPath(b.path("./raylib-5.5_linux_amd64/lib"));
-    exe.linkSystemLibrary("raylib");
+        exe.linkLibC();
+        exe.addIncludePath(b.path("./raylib-5.5_linux_amd64/include"));
+        exe.addLibraryPath(b.path("./raylib-5.5_linux_amd64/lib"));
+        exe.linkSystemLibrary("raylib");
 
-    b.installArtifact(exe);
+        b.installArtifact(exe);
+    }
 }
